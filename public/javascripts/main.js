0019e7b3cdabe9cd4b76468d080f7eadf811dab5
Forrest Smith <forrest216@gmail.com>
2019-08-04 23:06:04 -0700

    nav link animation tweak and buttonized external project links

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -3,8 +3,7 @@ $('document').ready(showLinks());
 function showLinks() {
    const navLinks = [...document.getElementsByClassName('pop-down')];
    navLinks.forEach((link, idx) => {
-      const i = idx + 2;
-      setTimeout(() => {link.setAttribute('style', 'top: 5vh')}, i * 500);
+      setTimeout(() => {link.setAttribute('style', 'top: 5vh')}, (idx + 2) * 500);
       setTimeout(() => {link.removeAttribute('style')}, 4000);
    });
 }
